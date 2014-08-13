d73151746d7ad9838f076be728cedff82d3fa194
ketlin.130896 <ketlin.130896@192.168.3.142>
2014-08-13 07:17:26 -0500

    Adicionando novo documento

---
+++ newEmptyPHP.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+* and open the tebhkgyiçfyçç6çf6f6t8fçffçf8iiiiiiiiiiiuihyuphuphyuihuihuhuihihuhuihiuhiuhihuihiuhiuhiuhhuhuhmplate in the editor.
+ */
+
